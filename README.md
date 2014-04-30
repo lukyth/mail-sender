44c214cf34720b6eeaa61796cafc2d6cd046335c
lukyth <k.sujautra@gmail.com>
2014-04-30 21:59:32 +0700

    Adding all the files.

---
+++ README.md
@@ -0,0 +1,24 @@
+## Installation
+
+  Clone the mail-sender repo:
+
+    $ git clone https://bitbucket.org/lukyth/mail-sender.git
+    $ cd mail-sender
+
+  Install dependencies:
+
+    $ npm install
+
+## Quick Start
+
+  Start Redis:
+
+    $ redis-server
+
+  Start the server:
+
+    $ node mail.js
+
+## Viewing Example
+
+  http://localhost:3000
---
+++ mail.js
@@ -0,0 +1,50 @@
+var nodemailer = require('nodemailer');
+var kue = require('kue');
+var jobs = kue.createQueue();
+
+var email = '';
+var password = '';
+var mailService = 'Gmail';
+
+var transport = nodemailer.createTransport('SMTP',{
+    service: mailService,
+    auth: {
+        user: email,
+        pass: password
+    }
+});
+
+var mail = {}
+
+var job = jobs.create('email', {
+    title: 'mail1',
+    to: 'koobitor@gmail.com'
+}).delay(10000).priority('high').save();
+
+for(var i = 2; i < 7; ++i){
+    jobs.create('email', {
+        title: 'mail'+i,
+        to: 'koobitor@gmail.com',
+        from: email
+    }).delay(10000 * i).priority('high').save();
+}
+
+jobs.promote();
+
+jobs.process('email', 10, function (job, done) {
+    mail.from = job.data.from;
+    mail.to = job.data.to;
+    mail.subject = job.data.title;
+    mail.html = "This is " + job.data.title;
+    transport.sendMail(mail, function(error, response){
+        if(error){
+            console.log(error);
+        }else{
+            console.log(job.data.title+' complete');
+        }
+    });
+    done();
+});
+
+kue.app.listen(3000);
+console.log('UI started on port 3000');
